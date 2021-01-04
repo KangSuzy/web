3ce57088603d11dbcfd091743065cf0ca6418ce9
KangSuzy <astnwl321@gmail.com>
2021-01-04 16:47:17 +0900

    JS comparison operator

--- nodejs/syntax/boolean.js
+++ nodejs/syntax/boolean.js
@@ -1,2 +1,10 @@
 console.log(true);
 console.log(false);
+
+//comparsion operator
+console.log(1==1); //true
+console.log(1==2); //false
+console.log(1>2); //false
+console.log(1<2); //false
+console.log(1===1); //true
+console.log(1===2); //false
