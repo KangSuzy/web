f45f13ba0c5b8026dc34e9240e9b9dd6c1d01525
KangSuzy <astnwl321@gmail.com>
2021-01-08 13:35:31 +0900

    App - 글 목록 출력하기

--- nodejs/main.js
+++ nodejs/main.js
@@ -8,34 +8,57 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url, true).pathname;
     var title = queryData.id;
  
-    if(pathname === '/'){
-      if(queryData.id === undefined){
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+  if(pathname === '/'){
+    if(queryData.id === undefined){
+
+      fs.readdir('./data', function(err,filelist){
+
+        var title = 'welcome';
+        var description = 'Hello Node.js!';
+
+        var list = '<ul>';
+        var i =0;
+        while(i<filelist.length){
+          list += `<li><a href = "/?id=${filelist[i]}">${filelist[i]}</a></li>`
+          i +=1;
+        }
+        list = list + '</ul>'
+
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          ${list}
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+      })
+
+      } else{
+
+        fs.readdir('./data', function(err,filelist){
+  
           var title = 'welcome';
           var description = 'Hello Node.js!';
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
-          response.writeHead(200);
-          response.end(template);
-        });
-      } else{
+  
+          var list = '<ul>';
+          var i =0;
+          while(i<filelist.length){
+            list += `<li><a href = "/?id=${filelist[i]}">${filelist[i]}</a></li>`
+            i +=1;
+          }
+          list = list + '</ul>'
+  
+
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
           var tilte = queryData.id;
           var template = `
@@ -47,11 +70,7 @@ var app = http.createServer(function(request,response){
           </head>
           <body>
             <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
+            ${list}
             <h2>${title}</h2>
             <p>${description}</p>
           </body>
@@ -60,13 +79,14 @@ var app = http.createServer(function(request,response){
           response.writeHead(200);
           response.end(template);
         });
-      }
-  
-    } else {
-      response.writeHead(404);
-      response.end('Not found');
+      });
     }
- 
+
+  } else {
+    response.writeHead(404);
+    response.end('Not found');
+  }
+
  
  
 });
