84841e94c905e7eb371d51818f981517dd90ad42
KangSuzy <astnwl321@gmail.com>
2021-01-18 14:56:37 +0900

    JS 객체와 반복문

--- nodejs/syntax/object.js
+++ nodejs/syntax/object.js
@@ -1,6 +1,11 @@
 var members = ['a','b','c'];
 console.log(members[1]);
 //배열
+var i = 0;
+while(i<members.length){
+    console.log('array loop',members[i]);
+    i +=1;
+}
 
 var roles = {
     'programmer':'a',
@@ -8,3 +13,8 @@ var roles = {
     'manager':'c'
 } //객체
 console.log(roles.manager);
+console.log(roles['manager']);
+
+for(var n in roles){
+    console.log('object =>', n, 'value =>', roles[n]);
+}
