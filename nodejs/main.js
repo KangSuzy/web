7d84d55479a62e33a7c81ffe74d2fc522165d22b
KangSuzy <astnwl321@gmail.com>
2021-01-18 14:44:43 +0900

    App - Delete 글삭세 기능 완성

--- nodejs/main.js
+++ nodejs/main.js
@@ -52,7 +52,13 @@ var app = http.createServer(function(request,response){
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
             var title = queryData.id;
             var list = templateList(filelist);
-            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`,`<a href="/create">create</a> <a href="/update?id=${title}">update</a>
+            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`,
+          ` <a href="/create">create</a> 
+            <a href="/update?id=${title}">update</a>
+            <form action="/delete_process" method="POST">
+              <input type="hidden" name="id" value="${title}">
+              <input type="submit" value="delete">
+            </form>
         `);
             response.writeHead(200);
             response.end(template);
@@ -120,6 +126,39 @@ var app = http.createServer(function(request,response){
           response.writeHead(200);
           response.end(template);
         });
+      });
+    } else if(pathname === '/update_process'){
+      var body ='';
+
+      request.on('data', function (data){
+        body += data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        var title = post.title;
+        var description = post.description;
+        fs.rename(`data/${id}`,`data/${title}`, function(err){
+            
+          fs.writeFile(`data/${title}`,description, 'utf8', function(err){
+            response.writeHead(302, {Location : `/?id=${title}`});
+            response.end();
+            })
+          });
+      });
+    } else if(pathname === '/delete_process'){
+      var body ='';
+
+      request.on('data', function (data){
+        body += data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        fs.unlink(`data/${id}`, function(err){
+          response.writeHead(302, {Location: `/`});
+          response.end();
+        })
       });
     } else {
       response.writeHead(404);
